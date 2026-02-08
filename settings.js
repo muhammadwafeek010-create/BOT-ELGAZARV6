912db0071063b68b1f50b054298a2cfdf39c23d7
muhammadwafeek010-create <muhammadwafeek010@gmail.com>
2026-02-08 10:46:24 +0200

    Refactor settings.js with updated global variables

--- settings.js
+++ settings.js
@@ -15,22 +15,17 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['201090222331'] //ur owner number
+global.ownername = "PHARAOH-BOT" //ur owner name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
+global.ownernomer = "201090222331" //ur number
+global.premium = ['201090222331'] //ur premium number
 global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
 global.themeemoji = "♥" //ur theme emoji
 global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
+global.author = "⍣01144969997⍣" //ur sticker watermark author
 global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
 
 // Other
@@ -128,3 +123,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 })
+
